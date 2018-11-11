51ba577630b5814bb7af7c4499d70dc6ffe264f9
Jarek Pawlak <jaroslaw.pawlak@ocado.com>
2018-11-11 21:10:52 +0100

    displaying first calendar days ;)

--- src/App.js
+++ src/App.js
@@ -1,13 +1,34 @@
 import React, { Component } from 'react';
 import './App.css';
+let moment = require('moment');
 
 class App extends Component {
+  buildDays(year, month) {
+    const firstOf = moment([year, month, 1]);
+    const days = [];
+    let current = moment(firstOf);
+    // TODO: refactor to generators
+    while (current.isSame(firstOf, 'month')) {
+      days.push(moment(current));
+      current.add(1, 'day');
+    }
+
+    return days;
+  }
+
+  renderDays() {
+    const now = moment();
+    return this.buildDays(now.year(), now.month()).map((d,i) =>
+      <div key={i}>{d.format('DD-MMM-Y')}</div>
+    )
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <p>
-          </p>
+          {this.renderDays()}
+
         </header>
       </div>
     );
